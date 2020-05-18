991376e9ce55a9d7748c29d8ba5f35c909602773
Zulaikha Zakiullah <36493962+zzakiullah@users.noreply.github.com>
2020-05-17 21:32:50 -0400

    Add files via upload

---
+++ Game.cpp
@@ -0,0 +1,17 @@
+// Game.cpp
+// Sheng Fang & Zulaikha Zakiullah
+// This instantiates the Game class, which...runs our game.
+
+#include <iostream>
+#include <SDL.h>
+
+class Game {
+
+
+public:
+
+	Game() {
+
+	}
+
+};
