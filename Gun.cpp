1d7a8e7da055e1231af1db387b2545a6174a22e8
rabiyamajeed <59377629+rabiyamajeed@users.noreply.github.com>
2020-07-04 18:32:17 -0400

    Update Gun.cpp

--- Gun.cpp
+++ Gun.cpp
@@ -23,7 +23,7 @@ int Gun::get_durability() { return Gun::durability; }
 double Gun::get_recoil() { return Gun::recoil; }
 
 void Gun::set_ammo(int ammo) { Gun::ammo = ammo; }
-void Gun::set_durability(int new_durability) { Gun::durability = durability; }
+void Gun::set_durability(int durability) { Gun::durability = durability; }
 void Gun::set_shooting_speed(int shooting_speed) { Gun::shooting_speed = shooting_speed; }
 void Gun::set_accuracy(int accuracy) { Gun::accuracy = accuracy; }
 void Gun::set_durability(int durability) { Gun::durability = durability; }
