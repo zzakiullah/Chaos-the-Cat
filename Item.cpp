bb0a5bde5c0d07464c6b6df824c09cd84a0184df
s-yfang <55638266+s-yfang@users.noreply.github.com>
2020-07-07 18:28:43 -0400

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -4,13 +4,18 @@
 
 #include "Item.h"
 
-Item::Item(std::string t) {
-	type = t;								// Specifies what class of item it is
-	itemID = 0;								// Item name/more specific 
-	durability = 0.0;
-	rarity = 0;
-	value = 0.0;							// Value of the item determines its price
-	price = value + rarity * durability;	// An example of how we could calculate it
+Item::Item(std::string st) {
+
+	// Split string from txt file and assign the stats
+	std::string * stats = splitStats(st);
+
+	type = stats[0];								// Specifies what class of item it is
+	itemID = std::stoi(stats[1]);					// Item name/more specific 
+	durability = std::stoi(stats[2]);
+	rarity = std::stoi(stats[3]);
+	value = std::stoi(stats[4]);					// Value of the item determines its price
+
+	price = value + rarity * durability;			// An example of how we could calculate it
 }
 
 Item::~Item() {}
@@ -30,4 +35,23 @@ void Item::set_rarity(int r) { rarity = r; }
 void Item::set_durability(double d) { durability = d; }
 void Item::set_value(double v) { value = v; }
 void Item::set_price(double p) { price = p; }
+
+// Splits up given string word by word and returns them in an array
+std::string * splitStats(std::string st) {
+	std::stringstream ssline(st);
+	std::string* stats = new std::string[5]{};
+
+	int i = 0;
+
+	while (ssline.good() && i < 5) {
+		ssline >> stats[i];
+		++i;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		std::cout << stats[i] << std::endl;
+	}
+
+	return stats;
+}
 
