e4bc627748267d56ce37c9a3845f5c6d92c018e4
s-yfang <55638266+s-yfang@users.noreply.github.com>
2020-07-07 17:55:44 -0400

    Added Item.cpp and Item.h

---
+++ Item.cpp
@@ -0,0 +1,33 @@
+// Item.cpp
+// Sheng Fang, Rabiya Majeed, & Zulaikha Zakiullah
+// This instantiates the item class. Item stats should be read off a txt. file (?)
+
+#include "Item.h"
+
+Item::Item(std::string t) {
+	type = t;								// Specifies what class of item it is
+	itemID = 0;								// Item name/more specific 
+	durability = 0.0;
+	rarity = 0;
+	value = 0.0;							// Value of the item determines its price
+	price = value + rarity * durability;	// An example of how we could calculate it
+}
+
+Item::~Item() {}
+
+// Getters
+std::string Item::get_type() { return type; }
+int Item::get_itemID() { return itemID; }
+int Item::get_rarity() { return rarity; }
+double Item::get_durability() { return durability; }
+double Item::get_value() { return value; }
+double Item::get_price() { return price; }
+
+// Setters
+void Item::set_type(std::string t) { type = t; }
+void Item::set_itemID(int id) { itemID = id; }
+void Item::set_rarity(int r) { rarity = r; }
+void Item::set_durability(double d) { durability = d; }
+void Item::set_value(double v) { value = v; }
+void Item::set_price(double p) { price = p; }
+
---
+++ Item.h
@@ -0,0 +1,43 @@
+#pragma once
+// Item.h
+// Sheng Fang, Rabiya Majeed, & Zulaikha Zakiullah
+// This instantiates the item class.
+
+#include <iostream>
+#include <SDL.h>
+#include "Object.h"
+#include <string>
+
+class Item : public Object{
+
+private:
+	std::string type;
+	int itemID;
+	double durability;
+	int rarity;
+	double value;
+	double price;
+
+public:
+
+	// Constructor and destructor
+	Item(std::string t);
+	~Item();
+
+	// Getters
+	std::string get_type();
+	int get_itemID();
+	int get_rarity();
+	double get_durability();
+	double get_value();
+	double get_price();
+
+	// Setters
+	void set_type(std::string t);
+	void set_itemID(int id);
+	void set_rarity(int r);
+	void set_durability(double d);
+	void set_value(double v);
+	void set_price(double p);
+
+};
