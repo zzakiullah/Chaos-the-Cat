40d4fc9ade4baeb169a87145cb335360fdf78672
s-yfang <55638266+s-yfang@users.noreply.github.com>
2020-07-17 23:39:29 -0400

    Update Object.cpp

--- Object.cpp
+++ Object.cpp
@@ -5,20 +5,20 @@
 #include "Object.h"
 
 Object::Object() {
-	Object::x = 0;
-	Object::y = 0;
-	Object::width = 0;
-	Object::height = 0;
+	x = 0;
+	y = 0;
+	width = 0;
+	height = 0;
 }
 
 Object::~Object() {}
 
-int Object::get_x() { return Object::x; }
-int Object::get_y() { return Object::y; }
-int Object::get_width() { return Object::width; }
-int Object::get_height() { return Object::height; }
+int Object::get_x() { return x; }
+int Object::get_y() { return y; }
+int Object::get_width() { return width; }
+int Object::get_height() { return height; }
 
-void Object::set_x(int new_x) { Object::x = new_x; }
-void Object::set_y(int new_y) { Object::y = new_y; }
-void Object::set_width(int w) { Object::width = w; }
-void Object::set_height(int h) { Object::height = h; }
+void Object::set_x(int new_x) { x = new_x; }
+void Object::set_y(int new_y) { y = new_y; }
+void Object::set_width(int w) { width = w; }
+void Object::set_height(int h) { height = h; }
