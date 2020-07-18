335c7c2b40f453004c1497680ba81e0d8146d773
s-yfang <55638266+s-yfang@users.noreply.github.com>
2020-07-17 23:41:03 -0400

    Added weight variable

--- Object.h
+++ Object.h
@@ -10,6 +10,7 @@ class Object {
 
 private:
 	int x, y, width, height;
+	double weight;
 
 public:
 	Object();
@@ -19,9 +20,11 @@ public:
 	int get_y();
 	int get_width();
 	int get_height();
-	
+	double get_weight();
+
 	void set_x(int new_x);
 	void set_y(int new_y);
 	void set_width(int w);
 	void set_height(int h);
+	void set_weight(double w);
 };
