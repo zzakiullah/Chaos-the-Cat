6ad93a4e786c6b6bc9598186e6271eb0c4d9c306
s-yfang <55638266+s-yfang@users.noreply.github.com>
2020-07-18 15:40:38 -0400

    changed var names

--- Item.cpp
+++ Item.cpp
@@ -6,7 +6,7 @@
 
 Item::Item(std::string st) {
 
-	statNum = 5;
+	stat_num = 5;
 
 	std::stringstream ssline(st);					// Convert stream into stringstream.
 	std::string* stats = new std::string[5]{};		
@@ -14,13 +14,13 @@ Item::Item(std::string st) {
 	int i = 0;
 
 	// Checks if the stream raises any errors and extracts the words to be put into the array. 
-	while (ssline.good() && i < statNum) {
+	while (ssline.good() && i < stat_num) {
 		ssline >> stats[i];
 		++i;
 	}
 
 	type = stats[0];								
-	itemID = std::stoi(stats[1]);					
+	id = std::stoi(stats[1]);					
 	durability = std::stod(stats[2]);
 	rarity = std::stod(stats[3]);
 	value = std::stod(stats[4]);					
@@ -34,7 +34,7 @@ Item::~Item() {}
 // Getters
 
 std::string Item::get_type() { return type; }
-int Item::get_itemID() { return itemID; }
+int Item::get_id() { return id; }
 double Item::get_rarity() { return rarity; }
 double Item::get_durability() { return durability; }
 double Item::get_value() { return value; }
@@ -43,7 +43,7 @@ double Item::get_price() { return price; }
 // Setters
 
 void Item::set_type(std::string t) { type = t; }
-void Item::set_itemID(int id) { itemID = id; }
+void Item::set_id(int new_id) { id = new_id; }
 void Item::set_rarity(double r) { rarity = r; }
 void Item::set_durability(double d) { durability = d; }
 void Item::set_value(double v) { value = v; }
